994708578b7b87767832f31111a2e86717ab6253
Wose <Jacksonteather@hotmail.com>
2018-04-27 10:32:35 -0400

    🛠 stubs for home component and methods module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from './shared/material.module';
+import { MethodsModule } from './methods/methods.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MethodsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: HomeComponent
+        }
+      ],
+      { enableTracing: true }
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
