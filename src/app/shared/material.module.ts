1d3b4c59cce7f876223080e48dd0d0c490d6ffde
Wose <Jacksonteather@hotmail.com>
2018-04-28 00:44:37 -0400

    📦 material icons

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {
   MatSidenavModule,
   MatExpansionModule,
+  MatIconModule,
   MatListModule,
   MatButtonModule
 } from '@angular/material';
 
 const imports = [MatSidenavModule, MatExpansionModule, MatListModule, MatButtonModule];
+  MatIconModule,
 
 @NgModule({
   imports,
