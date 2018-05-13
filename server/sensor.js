e8e7b63b45c2e9ea9542376c1b81e2c69c30c277
Wose <Jacksonteather@hotmail.com>
2018-05-13 11:15:43 -0400

    🛠 wip vibration switch neopixel activation

--- server/sensor.js
+++ server/sensor.js
@@ -1,2 +1,24 @@
+const PythonShell = require('python-shell');
 const Gpio = require('onoff').Gpio;
-const sensor = new Gpio(2, 'out');
+const sensor = new Gpio(4, 'in', 'rising', { debounceTimeout: 10 });
+
+let methodInUse = false;
+sensor.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+  if (!methodInUse && value === 1) {
+    methodInUse = true;
+    const args = { method: 'theaterChaseRainbow', clear: '' };
+    PythonShell.run(
+      'method.py',
+      { args, scriptPath: path.resolve(path.join(__dirname, 'scripts', 'neopixel')), mode: 'binary' },
+      err => err && console.error(err)
+    );
+    setTimeout(() => methodInUse = false, 5000);
+  }
+});
+
+process.on('SIGINT', function () {
+  sensor.unexport();
+});
