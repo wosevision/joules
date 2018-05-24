04f117d19fcf25764842c30a06da70066952b50c
Wose <Jacksonteather@hotmail.com>
2018-05-24 14:17:57 -0400

    ⚡️ multi form inputs

--- src/app/text-display/multi-line/multi-line.component.ts
+++ src/app/text-display/multi-line/multi-line.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+
+import { MethodsService, Method } from '../../core/methods.service';
+import { alphanumeric } from '../../shared/validators';
 
 @Component({
   selector: 'joul-multi-line',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./multi-line.component.scss']
 })
 export class MultiLineComponent implements OnInit {
+  fields = this.fb.group({
+    fieldValues: this.fb.array([])
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  get fieldValues(): FormArray {
+    return this.fields.get('fieldValues') as FormArray;
   }
 
+  constructor(private fb: FormBuilder, private methods: MethodsService) {}
+
+  ngOnInit() {
+    this.fieldValues.valueChanges.subscribe(console.log);
+  }
+
+  newValue() {
+    this.fieldValues.push(new FormControl('', alphanumeric()));
+  }
+
+  // submitText() {
+  //   if (this.fieldValues.valid) {
+  //     this.fieldValues.disable();
+  //     this.methods
+  //       .send(Method.SingleLine, [this.fieldValues.value])
+  //       .subscribe(value => this.fieldValues.enable());
+  //   }
+  // }
 }
