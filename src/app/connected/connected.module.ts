aa21f37dd1d69179d95095264291aa478b779f93
Wose <Jacksonteather@hotmail.com>
2018-05-28 16:36:30 -0400

    🛠 connected module and hashtag component stub

---
+++ src/app/connected/connected.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TwitterHashtagComponent } from './twitter-hashtag/twitter-hashtag.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TwitterHashtagComponent]
+})
+export class ConnectedModule { }
---
+++ src/app/connected/twitter-hashtag/twitter-hashtag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'joul-twitter-hashtag',
+  templateUrl: './twitter-hashtag.component.html',
+  styleUrls: ['./twitter-hashtag.component.scss']
+})
+export class TwitterHashtagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
