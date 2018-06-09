aaa16b1781e47c81c17ea24439a45b1f9ef63f7b
Wose <Jacksonteather@hotmail.com>
2018-06-09 12:10:58 -0400

    🚿 more folder restructuring

---
+++ server/scripts/scrollphat/connected/clear.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+
+import scrollphathd
+
+def clear_exit():
+    print 'Clearing display and exiting...'
+
+    # Clear the display
+    scrollphathd.clear()
+    sys.exit()
