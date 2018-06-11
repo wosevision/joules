d8f1a54ff09a3ec9d2553c4e835c989e35e5b135
Wose <Jacksonteather@hotmail.com>
2018-06-11 16:17:04 -0400

    ⚡️ display IP before connection

--- server/api.js
+++ server/api.js
@@ -1,21 +1,33 @@
 const path = require('path');
 const https = require('https');
 const express = require('express');
-// const Gpio = require('onoff').Gpio;
+const ip = require('ip');
 const PythonShell = require('python-shell');
 
 const router = express.Router();
 
 const routes = require('../common/routes.json');
+const scriptPath = path.resolve(path.join(__dirname, 'scripts'));
 
-const mockResponse = (real, mock) => process.env.NODE_ENV === 'production' ? real() : () => setTimeout(mock, 1500);
+let currentScript = PythonShell.run(
+  `scrollphat/text-display/single-line.py`,
+  { args: [ip.address()], scriptPath, mode: 'binary' },
+  handleError
+);
 
-let currentScript;
-const cleanup = () => currentScript && currentScript.terminate && currentScript.terminate();
-const monitor = (script, response) => {
+function cleanup() {
+  currentScript && currentScript.terminate && currentScript.terminate();
+}
+function monitor(script, response) {
   script.once('message', message => response.status(200).send({ message }));
   script.once('error', error => response.status(400).send(error));
 };
+function handleError(error) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
 
 const makeRoutes = (paths, group) =>
   paths.map(p => p.group
@@ -24,30 +36,18 @@ const makeRoutes = (paths, group) =>
       const args = req.body.message;
       // response
       if (args && Array.isArray(args)) {
-        mockResponse(
-          () => {
-            cleanup();
-            currentScript = PythonShell.run(
-              `${p}.py`,
-              { args, scriptPath: path.resolve(path.join(__dirname, 'scripts', group)), mode: 'binary' },
-              error => {
-                if (error) {
-                  console.error(error);
-                  // res.status(500).send({ success: false, error });
-                  process.exit(1);
-                }
-              }
-            );
-            res.status(200).json({ success: true });
-          },
-          // mock
-          () =>
-            res.status(200).json({
-              success: true,
-              mock: true,
-              body: req.body
-            })
+        cleanup();
+        currentScript = PythonShell.run(
+          `${p}.py`,
+          { args, scriptPath: path.join(scriptPath, group), mode: 'binary' },
+          error => {
+            if (error) {
+              console.error(error);
+              process.exit(1);
+            }
+          }
         );
+        res.status(200).json({ success: true });
       } else {
         res.status(400).send({ success: false, message: 'invalid arguments'})
       }
